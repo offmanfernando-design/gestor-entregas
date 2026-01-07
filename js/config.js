4b4792380a608ab565b4ffba437b8b6a5d1acf09
offmanfernando-design <offman.fernando@gmail.com>
2026-01-07 10:09:22 -0400

    config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
-const API_BASE = "https://script.google.com/macros/s/TU_DEPLOY_ID/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbxs8r1iwpElKRQOaM6txicvQ9TZFLi2UQEnRU3dbLn4Fp4m-iyWHwogwngMsH6WTHXd/exec";
+
